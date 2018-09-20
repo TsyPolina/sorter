db02654d6a78aa4acae95bef52cbb02dd598f5c8
TsyPolina <tsy.polina@gmail.com>
2018-09-21 02:26:16 +0300

    third task

--- src/index.js
+++ src/index.js
@@ -1,30 +1,53 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.array = [];
+    this.customSortFunction = function(a, b) {
+      return a - b;
+    }
+    this.customSort = function(array, indices) {
+      for (let i = 0; i < array.length; i++) {
+          for (let j = i; j < array.length; j++) {
+              if (!indices) {
+                  if (this.customSortFunction(array[i], array[j])) {
+                      let temp = array[i];
+                      array[i] = array[j];
+                      array[j] = temp;
+                  }
+              } else {
+                  if (indices.indexOf(i) !== -1 && indices.indexOf(j) !== -1 && this.customSortFunction(array[i], array[j]) > 0) {
+                      let temp = array[i];
+                      array[i] = array[j];
+                      array[j] = temp;
+                  }
+              }
+          }
+      }
+      this.arr = array;
+    }
   }
 
   add(element) {
-    // your implementation
+    this.array.push(element);
   }
 
   at(index) {
-    // your implementation
+    return this.array[index];
   }
 
   get length() {
-    // your implementation
+    return this.array.length;
   }
 
   toArray() {
-    // your implementation
+    return this.array;
   }
 
   sort(indices) {
-    // your implementation
+    this.customSort(this.array, indices);
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.customSortFunction = compareFunction;
   }
 }
 
